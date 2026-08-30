56a73741cb3a79da5b0c25df4a4dd93e6514f897
NexiCH86 <luis.moran@mail.ch>
2026-08-30 17:10:12 +0200

    feat: open LuMa Radar from launcher

---
+++ luma-radar-launcher/src/main/java/ch/lumalabs/radar/MainActivity.java
@@ -0,0 +1,19 @@
+package ch.lumalabs.radar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static final Uri TARGET = Uri.parse("https://lumalabs.ch/radar");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(Intent.ACTION_VIEW, TARGET);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        startActivity(intent);
+        finish();
+    }
+}
